0aa1a4a4419326636181c63d64116809c28206f5
Sam Odum <sam.odum1@gmail.com>
2020-01-06 23:48:22 +0100

    ch(route): define endpoints for agent creation/deletion

--- server/routes/apiV1/authRoute.js
+++ server/routes/apiV1/authRoute.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
 const { Auth } = require('../../middleware/Auth');
 const employees = require('../../controllers/employees');
+
 const router = new Router();
-const space = ''
+
 router.post("/supr", Auth.isSuperAdmin, employees.createBare);
-router.post('/create-user', Auth.verifyToken, Auth.isAdmin, employees.createBare);
-router.delete('/delete-user', Auth.verifyToken, Auth.isAdmin, employees.deleteBare);
+router.post('/create-agent', Auth.verifyToken, Auth.isAdmin, employees.createBare);
+router.delete('/delete-agent', Auth.verifyToken, Auth.isAdmin, employees.deleteBare);
 
 module.exports = routers
